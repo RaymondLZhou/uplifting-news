ae8552ac091a992ceb4dbec0be43c8b716488b11
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-01 18:50:28 -0400

    Added json file to write

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,15 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
-import feedparser
+import feedparser as fp
 import numpy as np
 import pandas as pd
+import json
 
 url = "http://feeds.bbci.co.uk/news/rss.xml"
-feed = feedparser.parse(url)
+feed = fp.parse(url)
+
+newsList = []
 
 for elem in feed.entries:
-    print(elem.title)
-    print(elem.description)
-    print(elem.published)
-    print(elem.link)
-    print()
+    newsObject = {
+        "title": elem.title,
+        "description": elem.description,
+        "date": elem.published,
+        "link": elem.link
+    }
+    
+    newsList.append(newsObject)
+
+with open("raw.json", "w") as outfile:
+    json.dump(newsList, outfile, ensure_ascii=False, indent=4)
