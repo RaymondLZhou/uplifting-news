52335db611cebc762f1ed4de8e0c8a182656fa3e
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-02 21:05:35 -0400

    Added cleaning to articles

---
+++ src/scraper/cleaner.py
@@ -0,0 +1,18 @@
+import json
+import re
+
+with open("articlesRaw.json") as json_data:
+    newsList = json.load(json_data)
+
+REPLACE_NO_SPACE = re.compile(r"[.;:!\'?,\"()\[\]]")
+REPLACE_WITH_SPACE = re.compile(r"(<br\s*/><br\s*/>)|(\-)|(\/)")
+
+newsList = [news["text"] for news in newsList]
+
+newsList = [REPLACE_NO_SPACE.sub("", news.lower()) for news in newsList]
+newsList = [REPLACE_WITH_SPACE.sub(" ", news) for news in newsList]
+
+articles = [{"text": news} for news in newsList]
+
+with open("articlesCleaned.json", "w") as outfile:
+    json.dump(articles, outfile, indent=4)
