2703ae322f8836902feadc541f90e683e3d25a0e
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-03 20:16:00 -0400

    Added temp display to posts

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
+import GetLocalPosts from './displayFeed'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <Link to="/second-page">
-          <button className="button">
-           Link to Second Page
-          </button>
-        </Link>
-      </header>
-    </div>
-  )
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+  
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+  
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+  
+          <Link to="/second-page">
+            <button className="button">
+             Link to Second Page
+            </button>
+          </Link>
+  
+          <GetLocalPosts/>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
