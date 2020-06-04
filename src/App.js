bb52bc232866122c4d7cf5ae37ca6492f9370cc5
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-03 20:19:50 -0400

    Restored for new approach

--- src/App.js
+++ src/App.js
@@ -2,39 +2,34 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
-import GetLocalPosts from './displayFeed'
 
-class App extends Component {
-  render () {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-  
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-  
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-  
-          <Link to="/second-page">
-            <button className="button">
-             Link to Second Page
-            </button>
-          </Link>
-  
-          <GetLocalPosts/>
-        </header>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <Link to="/second-page">
+          <button className="button">
+           Link to Second Page
+          </button>
+        </Link>
+      </header>
+    </div>
+  )
 }
 
 export default App
