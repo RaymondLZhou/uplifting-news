053b2acec8283e9a1b61f5c5ed531626b05263e5
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-05 19:29:36 -0400

    Updated SecondPage to be class

--- src/SecondPage.js
+++ src/SecondPage.js
@@ -3,33 +3,35 @@ import { Link } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
 
-function SecondPage() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          This is a Second Page!
-        </p>
-        
-        <Link to="/">
-          <button className="button" id="spaces">
-           Link to First Page
-          </button>
-        </Link>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+class SecondPage extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+  
+          <p>
+            This is a Second Page!
+          </p>
+          
+          <Link to="/">
+            <button className="button" id="spaces">
+             Link to First Page
+            </button>
+          </Link>
+  
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default SecondPage
