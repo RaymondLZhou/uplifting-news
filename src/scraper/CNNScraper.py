4a3517f49fed78606cc14e4d1f905ba4b6093a51
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-06 17:03:59 -0400

    Created CNN scraper

--- src/scraper/CNNScraper.py
+++ src/scraper/CNNScraper.py
@@ -1,20 +1,30 @@
 import feedparser as fp
 import json
 
-url = "http://feeds.bbci.co.uk/news/rss.xml"
+url = "http://rss.cnn.com/rss/cnn_topstories.rss"
 feed = fp.parse(url)
 
 newsList = []
 
 for elem in feed.entries:
+
+    title = elem.title if 'title' in elem.keys() else ""
+    description = elem.summary.split('<')[0] if 'summary' in elem.keys() else ""
+    date = elem.published if 'published' in elem.keys() else ""
+    link = elem.link if 'link' in elem.keys() else ""
+
+    if "" in (title, description, date, link):
+        continue
+    
+
     newsObject = {
-        "title": elem.title,
-        "description": elem.description,
-        "date": elem.published,
-        "link": elem.link
+        "title": title,
+        "description": description,
+        "date": date,
+        "link": link
     }
         
     newsList.append(newsObject)
 
-with open("BBCFeed.json", "w") as outfile:
+with open("CNNFeed.json", "w") as outfile:
     json.dump(newsList, outfile, indent=4)
