65d608dcd065187652933f9ecf602288266ac16b
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-10 21:35:47 -0400

    Created csv output

--- src/sentiment/vader.py
+++ src/sentiment/vader.py
@@ -1,12 +1,32 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import json
+import pandas as pd
 
 with open("../scraper/bbc/BBCFeed.json") as json_data:
     newsList = json.load(json_data)
 
 sia = SentimentIntensityAnalyzer()
+descriptions = []
+positive = []
+negative = []
+neutral = []
+overall = []
 
 for news in newsList:
     scores = sia.polarity_scores(news["description"])
-    print("{:-<40} {}".format(news["description"], str(scores)))
-    print()
+    
+    descriptions.append(news["description"])
+    positive.append(scores["pos"])
+    neutral.append(scores["neu"])
+    negative.append(scores["neg"])
+    overall.append(scores["compound"])
+
+frame = pd.DataFrame ({
+    "description": descriptions,
+    "positive": positive,
+    "neutral": neutral,
+    "negative": negative,
+    "overall": overall,
+})
+
+frame.to_csv("BBCSentiment.csv")
