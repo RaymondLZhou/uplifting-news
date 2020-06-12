fa84a4c3aceb190a4feb862643ed9652c73952d8
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-11 20:50:22 -0400

    Refactored code

--- src/sentiment/vader.py
+++ src/sentiment/vader.py
@@ -2,60 +2,42 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import json
 import pandas as pd
 
+def createCSV(newsList):
+    sia = SentimentIntensityAnalyzer()
+    descriptions = []
+    positive = []
+    negative = []
+    neutral = []
+    overall = []
+
+    for news in newsList:
+        scores = sia.polarity_scores(news["title"] + news["description"])
+        
+        descriptions.append(news["description"])
+        positive.append(scores["pos"])
+        neutral.append(scores["neu"])
+        negative.append(scores["neg"])
+        overall.append(scores["compound"])
+
+    sentiments = pd.DataFrame ({
+        "description": descriptions,
+        "positive": positive,
+        "neutral": neutral,
+        "negative": negative,
+        "overall": overall,
+    })
+
+    return sentiments
+
+
 with open("../scraper/bbc/BBCFeed.json") as json_data:
     newsList = json.load(json_data)
 
-sia = SentimentIntensityAnalyzer()
-descriptions = []
-positive = []
-negative = []
-neutral = []
-overall = []
-
-for news in newsList:
-    scores = sia.polarity_scores(news["title"] + news["description"])
-    
-    descriptions.append(news["description"])
-    positive.append(scores["pos"])
-    neutral.append(scores["neu"])
-    negative.append(scores["neg"])
-    overall.append(scores["compound"])
-
-sentiments = pd.DataFrame ({
-    "description": descriptions,
-    "positive": positive,
-    "neutral": neutral,
-    "negative": negative,
-    "overall": overall,
-})
-
+sentiments = createCSV(newsList)
 sentiments.to_csv("BBCSentiment.csv")
 
 with open("../scraper/cnn/CNNFeed.json") as json_data:
     newsList = json.load(json_data)
 
-sia = SentimentIntensityAnalyzer()
-descriptions = []
-positive = []
-negative = []
-neutral = []
-overall = []
-
-for news in newsList:
-    scores = sia.polarity_scores(news["title"] + news["description"])
-    
-    descriptions.append(news["description"])
-    positive.append(scores["pos"])
-    neutral.append(scores["neu"])
-    negative.append(scores["neg"])
-    overall.append(scores["compound"])
-
-sentiments = pd.DataFrame ({
-    "description": descriptions,
-    "positive": positive,
-    "neutral": neutral,
-    "negative": negative,
-    "overall": overall,
-})
-
+sentiments = createCSV(newsList)
 sentiments.to_csv("CNNSentiment.csv")
