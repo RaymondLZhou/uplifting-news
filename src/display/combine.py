908583650ca2063ee72b0c4ed59553294c8bdef0
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-13 19:25:25 -0400

    Sorted feed based on positivity

--- src/display/combine.py
+++ src/display/combine.py
@@ -7,6 +7,9 @@ with open("../sentiment/CNNDisplayFeed.json") as json_data:
     CNNFeed = json.load(json_data)
 
 displayFeed = BBCFeed + CNNFeed
+displayFeed.sort(key=lambda x: x['overall'])
+displayFeed.reverse()
 
-with open("DisplayFeed.json", "w") as outfile:
+with open("displayFeed.json", "w") as outfile:
     json.dump(displayFeed, outfile, indent=4)
+    
