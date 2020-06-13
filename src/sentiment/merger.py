0973623bb008c8339803c0c228fab33018d12863
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-12 22:30:25 -0400

    Created merging of both json files

--- src/sentiment/merger.py
+++ src/sentiment/merger.py
@@ -1,27 +1,43 @@
 import json
 
+def mergeFeed(information, sentiments):
+    displayFeed = []
+
+    for feed, sentiment in zip(information, sentiments):
+        display = {
+            "title": feed["title"],
+            "description": feed["description"],
+            "date": feed["date"],
+            "link": feed["link"],
+            "positive": sentiment["positive"],
+            "neutral": sentiment["neutral"],
+            "negative": sentiment["negative"],
+            "overall": sentiment["overall"]
+        }
+
+        displayFeed.append(display)
+    
+    return displayFeed
+
 with open("../scraper/bbc/BBCFeed.json") as json_data:
     BBCFeed = json.load(json_data)
 
 with open("BBCSentiment.json") as json_data:
     BBCSentiment = json.load(json_data)
 
-displayFeed = []
-
-for feed, sentiment in zip(BBCFeed, BBCSentiment):
-    display = {
-        "title": feed["title"],
-        "description": feed["description"],
-        "date": feed["date"],
-        "link": feed["link"],
-        "description": sentiment["description"],
-        "positive": sentiment["positive"],
-        "neutral": sentiment["neutral"],
-        "negative": sentiment["negative"],
-        "overall": sentiment["overall"]
-    }
-
-    displayFeed.append(display)
+displayFeed = mergeFeed(BBCFeed, BBCSentiment)
 
 with open("BBCDisplayFeed.json", "w") as outfile:
+    json.dump(displayFeed, outfile, indent=4)
+
+
+with open("../scraper/cnn/CNNFeed.json") as json_data:
+    CNNFeed = json.load(json_data)
+
+with open("CNNSentiment.json") as json_data:
+    CNNSentiment = json.load(json_data)
+
+displayFeed = mergeFeed(CNNFeed, CNNSentiment)
+
+with open("CNNDisplayFeed.json", "w") as outfile:
     json.dump(displayFeed, outfile, indent=4)
