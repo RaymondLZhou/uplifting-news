0611563a677f5a1fa76822ab8fcb95cd55f6d597
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-16 19:17:09 -0400

    Created updated vectorizer

--- src/sentiment/process.py
+++ src/sentiment/process.py
@@ -1,13 +1,26 @@
 import json
-import nltk
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
 
 with open("../data/BBCArticlesCleaned.json") as json_data:
     newsList = json.load(json_data)
 
-stopwords = nltk.corpus.stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
+newsList = [news["text"] for news in newsList]
 
-first = newsList[0]["text"]
-first = nltk.word_tokenize(first)
-first = [word for word in first if word not in stopwords]
+def print_top_words(model, feature_names, n_top_words):
+    for index, topic in enumerate(model.components_):
+        message = "\nTopic #{}:".format(index)
+        message += " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1 :-1]])
+        print(message)
+        print("="*70)
 
-print(first)
+class LemmaCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(LemmaCountVectorizer, self).build_analyzer()
+        return lambda doc: (lemmatizer.lemmatize(w) for w in analyzer(doc))
+
+tf_vectorizer = LemmaCountVectorizer(max_df=0.95, min_df=2, stop_words='english', decode_error='ignore')
+tf = tf_vectorizer.fit_transform(newsList)
+
+print(tf)
