2efc7e2bdaa1124295f2fea74d910334cdcfba84
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-16 20:45:49 -0400

    Updated to csv

--- src/sentiment/topic.py
+++ src/sentiment/topic.py
@@ -11,7 +11,7 @@ newsList = [news["text"] for news in newsList]
 
 def print_top_words(model, feature_names, n_top_words):
     for index, topic in enumerate(model.components_):
-        message = "\nTopic #{}:".format(index)
+        message = "\nTopic #{}: ".format(index)
         message += " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1 :-1]])
         print(message)
         print("="*70)
