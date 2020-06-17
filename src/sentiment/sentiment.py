f70e4cf941813b8644aaab4c555a0232ba8c08ca
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-16 21:19:50 -0400

    Updated sentiment to include topics

--- src/sentiment/sentiment.py
+++ src/sentiment/sentiment.py
@@ -1,6 +1,23 @@
 import json
 import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+
+lemmatizer = WordNetLemmatizer()
+
+def print_top_words(model, feature_names, n_top_words):
+    for index, topic in enumerate(model.components_):
+        message = "\nTopic #{}: ".format(index)
+        message += " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1 :-1]])
+        print(message)
+        print("="*70)
+
+class LemmaCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(LemmaCountVectorizer, self).build_analyzer()
+        return lambda doc: (lemmatizer.lemmatize(w) for w in analyzer(doc))
 
 def createJSON(newsList):
     sia = SentimentIntensityAnalyzer()
@@ -26,7 +43,42 @@ def createJSON(newsList):
 
 df = pd.read_csv("../data/feed.csv")
 newsList = df.to_dict('records')
-sentiments = createJSON(newsList)
+
+articles = [news["text"] for news in newsList]
+tf_vectorizer = LemmaCountVectorizer(max_df=0.95, min_df=2, stop_words='english', decode_error='ignore')
+tf = tf_vectorizer.fit_transform(articles)
+
+lda = LatentDirichletAllocation(n_components=11, max_iter=5, learning_method = 'online', learning_offset = 50., random_state = 0)
+lda.fit(tf)
+
+n_top_words = 40
+tf_feature_names = tf_vectorizer.get_feature_names()
+# print_top_words(lda, tf_feature_names, n_top_words)
+
+vader = createJSON(newsList)
+messages = []
+
+for index, topic in enumerate(lda.components_):
+    message = ""
+    message += " ".join([tf_feature_names[i] for i in topic.argsort()[:-n_top_words - 1 :-1]])
+    messages.append(message)
+
+sentiments = []
+
+for v, message in zip(vader, messages):
+    sentiment = {
+        "title": v["title"],
+        "description": v["description"],
+        "date": v["date"],
+        "link": v["link"],
+        "positive": v["positive"],
+        "neutral": v["neutral"],
+        "negative": v["negative"],
+        "overall": v["overall"],
+        "keywords": message,
+    }
+
+    sentiments.append(sentiment)
 
 sentiments.sort(key=lambda x: x['overall'])
 sentiments.reverse()
