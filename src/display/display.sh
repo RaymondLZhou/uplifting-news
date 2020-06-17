91b5b40089d75d433d04c969948666d9730dc8d9
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-16 21:28:48 -0400

    Updated script

--- src/display/display.sh
+++ src/display/display.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
-python ../scraper/BBCScraper.py
-python ../scraper/CNNScraper.py
-
-python ../scraper/BBCRetriever.py
-python ../scraper/CNNRetriever.py
-
-python ../scraper/cleaner.py
-
-python ../sentiment/vader.py
-
-python combine.py
+python ../scraper/scraper.py
+python ../sentiment/sentiment.py
