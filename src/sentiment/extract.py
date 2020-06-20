a2081c538882768a5b3702946da2f4064e28beaf
Raymond Zhou <raymondzhou2019@gmail.com>
2020-06-19 20:45:36 -0400

    Updated keywords number

--- src/sentiment/extract.py
+++ src/sentiment/extract.py
@@ -4,7 +4,7 @@ def keywords(lda, tf_feature_names):
 
     for _, topic in enumerate(lda.components_):
         message = [tf_feature_names[i] for i in topic.argsort()[:-n_top_words - 1 :-1] if tf_feature_names[i] not in {"say", "said"}]
-        message = message[:6]
+        message = message[:5]
         message = ", ".join(message)
 
         messages.append(message)
