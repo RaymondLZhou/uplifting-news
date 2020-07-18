9643c8d5c9bced8c8106c3f983873f88abf08a63
Raymond Zhou <raymondzhou2019@gmail.com>
2020-07-17 20:21:44 -0400

    Loaded dataset

---
+++ src/sentiment/learnTemp.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+
+def plot_graphs(history, metric):
+    plt.plot(history.history[metric])
+    plt.plot(history.history['val_'+metric], '')
+    plt.xlabel("Epochs")
+    plt.ylabel(metric)
+    plt.legend([metric, 'val_'+metric])
+    plt.show()
+
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+encoder = info.features['text'].encoder
+print('Vocabulary size: {}'.format(encoder.vocab_size))
+
+sample_string = 'Hello TensorFlow.'
+
+encoded_string = encoder.encode(sample_string)
+print('Encoded string is {}'.format(encoded_string))
+
+original_string = encoder.decode(encoded_string)
+print('The original string: "{}"'.format(original_string))
+
+assert original_string == sample_string
+
+for index in encoded_string:
+    print('{} ----> {}'.format(index, encoder.decode([index])))
