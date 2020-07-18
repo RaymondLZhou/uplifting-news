c88c3675ff08d670a35d2e3005d5cedd49a7b06c
Raymond Zhou <raymondzhou2019@gmail.com>
2020-07-18 10:34:19 -0400

    Cleaned training code

--- src/sentiment/train.py
+++ src/sentiment/train.py
@@ -10,27 +10,14 @@ def plot_graphs(history, metric):
     plt.legend([metric, 'val_'+metric])
     plt.show()
 
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
 dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
 train_dataset, test_dataset = dataset['train'], dataset['test']
 
 encoder = info.features['text'].encoder
 print('Vocabulary size: {}'.format(encoder.vocab_size))
-
-sample_string = 'Hello TensorFlow.'
-
-encoded_string = encoder.encode(sample_string)
-print('Encoded string is {}'.format(encoded_string))
-
-original_string = encoder.decode(encoded_string)
-print('The original string: "{}"'.format(original_string))
-
-assert original_string == sample_string
-
-for index in encoded_string:
-    print('{} ----> {}'.format(index, encoder.decode([index])))
-
-BUFFER_SIZE = 10000
-BATCH_SIZE = 64
 
 train_dataset = train_dataset.shuffle(BUFFER_SIZE)
 train_dataset = train_dataset.padded_batch(BATCH_SIZE)
@@ -56,30 +43,6 @@ test_loss, test_acc = model.evaluate(test_dataset)
 
 print('Test Loss: {}'.format(test_loss))
 print('Test Accuracy: {}'.format(test_acc))
-
-def pad_to_size(vec, size):
-    zeros = [0] * (size - len(vec))
-    vec.extend(zeros)
-    return vec
-
-def sample_predict(sample_pred_text, pad):
-    encoded_sample_pred_text = encoder.encode(sample_pred_text)
-
-    if pad:
-        encoded_sample_pred_text = pad_to_size(encoded_sample_pred_text, 64)
-    
-    encoded_sample_pred_text = tf.cast(encoded_sample_pred_text, tf.float32)
-    predictions = model.predict(tf.expand_dims(encoded_sample_pred_text, 0))
-
-    return (predictions)
-
-sample_pred_text = ('The movie was cool. The animation and the graphics were out of this world. I would recommend this movie.')
-predictions = sample_predict(sample_pred_text, pad=True)
-print(predictions)
-
-sample_pred_text = ('The movie was not good. The animation and the graphics were terrible. I would not recommend this movie.')
-predictions = sample_predict(sample_pred_text, pad=True)
-print(predictions)
 
 plot_graphs(history, 'accuracy')
 plot_graphs(history, 'loss')
